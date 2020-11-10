b729c0b9709f8b4aa72bd1f63d58cfe9676da142
Fantomatique <70448638+Fantomatique@users.noreply.github.com>
2020-11-10 21:52:20 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js'); 
-
-const client = new Discord.Client(); 
-
-client.once('ready',() => {
-    console.log('Ready!');
-});
-
-client.login('Nzc0OTk1OTUyNjMzMTE4NzIw.X6f5OQ.Yta5jGGjT7aarZMPndPudY0uisw');
